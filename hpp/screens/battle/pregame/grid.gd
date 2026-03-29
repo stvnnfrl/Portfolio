d5d10bbd15031c87fe12f6c186729c58342dc21f
Jakob Adalin <jakob.adalin@mail.mcgill.ca>
2026-03-29 14:19:07 -0400

    made pregame use cubic_coords.gd and made the round function return a vector3i

--- hpp/screens/battle/pregame/grid.gd
+++ hpp/screens/battle/pregame/grid.gd
@@ -1,6 +1,6 @@
 extends ColorRect
 
-@export var grid_size: float = 128
+@export var cubic_coords: CubicCoords
 
 var placed_units: Dictionary[Vector3i, Node2D]
 
@@ -22,7 +22,7 @@ func _on_gui_input(event: InputEvent) -> void:
 		if current_unit < 0:
 			return
 		
-		var grid_coordinates = cubic_round(pos2D_to_cubic(event.position))
+		var grid_coordinates = cubic_coords.cubic_round(cubic_coords.pos2D_to_cubic(event.position))
 		var unit_at_click = placed_units.get(grid_coordinates)
 		
 		# free what was previously there
@@ -30,13 +30,13 @@ func _on_gui_input(event: InputEvent) -> void:
 			unit_at_click.queue_free()
 		
 		# place a new unit
-		var screen_coordinates = cubic_to_pos2D(grid_coordinates)
+		var screen_coordinates = cubic_coords.cubic_to_pos2D(grid_coordinates)
 		var new_unit = create_unit_at(screen_coordinates)
 		placed_units[grid_coordinates] = new_unit
 	
 	elif event.button_index == MouseButton.MOUSE_BUTTON_RIGHT:
 		# clear the unit
-		var grid_coordinates = cubic_round(pos2D_to_cubic(event.position))
+		var grid_coordinates = cubic_coords.cubic_round(cubic_coords.pos2D_to_cubic(event.position))
 		var unit_at_click = placed_units.get(grid_coordinates)
 		
 		if unit_at_click != null:
@@ -52,7 +52,7 @@ func create_unit_at(screen_coordinates: Vector2) -> Node2D:
 	
 	# make sizes consistent between units
 	var current_size = texture.get_size()
-	sprite.scale = Vector2(grid_size / current_size.x, grid_size / current_size.y)
+	sprite.scale = Vector2(cubic_coords.size / current_size.x, cubic_coords.size / current_size.y)
 	
 	sprite.position = screen_coordinates
 	
@@ -65,34 +65,3 @@ func _on_unit_selector_selected_unit_changed(index: int) -> void:
 
 func init(units_: Array[Variant]) -> void:
 	units = units_
-
-
-# coordinate conversion
-# from hex movement mini-project
-
-const RIGHT = Vector3(1, -1, 0)
-const DOWNRIGHT = Vector3(1, 0 ,-1)
-const DOWNLEFT = Vector3(0, 1, -1)
-
-const down_cubic = Vector3(1, 1, -2)/3
-const right_cubic = RIGHT/sqrt(3)
-func pos2D_to_cubic(pos: Vector2) -> Vector3:
-	return (pos.x * right_cubic + pos.y * down_cubic)/grid_size
-
-const x_2d = Vector2(sqrt(3)/2, 0.5)
-const y_2d = Vector2(-sqrt(3)/2, 0.5)
-const z_2d = Vector2(0, -1)
-func cubic_to_pos2D(pos: Vector3) -> Vector2:
-	return grid_size * (x_2d * pos.x + y_2d * pos.y + z_2d * pos.z)
-
-func cubic_round(pos: Vector3) -> Vector3i:
-	# credit to redblobgames.com/grids/hexagons
-	var rounded = round(pos)
-	var diff = abs(rounded - pos)
-	if (diff.x > diff.y && diff.x > diff.z):
-		rounded.x = -rounded.y-rounded.z
-	elif (diff.y > diff.z):
-		rounded.y = -rounded.x-rounded.z
-	else:
-		rounded.z = -rounded.x-rounded.y
-	return Vector3i(rounded)
