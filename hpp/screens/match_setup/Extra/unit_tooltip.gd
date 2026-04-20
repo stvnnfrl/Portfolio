466e75e3c61d9a8524a4e059a65995a98c11b6a5
Vincent Breau <93932211+Vincebro227@users.noreply.github.com>
2026-04-20 07:27:20 -0400

    warning fixes

--- hpp/screens/match_setup/Extra/unit_tooltip.gd
+++ hpp/screens/match_setup/Extra/unit_tooltip.gd
@@ -4,8 +4,8 @@ extends Control
 @onready var name_label: Label = $Panel/MarginContainer/HBoxContainer/VBoxContainer/Name_Label
 @onready var descriptive_label: Label = $Panel/MarginContainer/HBoxContainer/VBoxContainer/Decriptive_Label
 
-func set_unit(name: String, texture: Texture2D, desc: String) -> void:
-	name_label.text = name
+func set_unit(unit_name: String, texture: Texture2D, desc: String) -> void:
+	name_label.text = unit_name
 	icon.texture = texture
 	descriptive_label.text = desc
 	
